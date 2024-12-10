5bc5786c2845fd7058efc7059d7523800f31d563
kokolybe <52601492+kokolybe@users.noreply.github.com>
2024-12-10 21:49:15 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#7B2CBF',
+        secondary: '#FF9F1C',
+        accent: '#48CAE4',
+        rose: '#FFB5E8',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
